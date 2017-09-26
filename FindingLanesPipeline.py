9ec36735f3ab2b13eb00211fba52ad916f467cf7
scifiss <rebecca.shuang@gmail.com>
2017-09-26 08:18:14 -0500

    Update FindingLanesPipeline.py

--- FindingLanesPipeline.py
+++ FindingLanesPipeline.py
@@ -96,42 +96,7 @@ def mag_thresh(img, sobel_kernel=3, thresh=(0, 255)):
     return binary_output
 
 
-## Define a function that takes an image, number of x and y points, 
-## camera matrix and distortion coefficients
-#def corners_unwarp(img, nx, ny, mtx, dist):
-#    # Use the OpenCV undistort() function to remove distortion
-#    undist = cv2.undistort(img, mtx, dist, None, mtx)
-#    # Convert undistorted image to grayscale
-#    gray = cv2.cvtColor(undist, cv2.COLOR_RGB2GRAY)
-#    # Search for corners in the grayscaled image
-#    ret, corners = cv2.findChessboardCorners(gray, (nx, ny), None)
-#
-#    if ret == True:
-#        # If we found corners, draw them! (just for fun)
-#        image_corners=cv2.drawChessboardCorners(undist, (nx, ny), corners, ret)
-#        misc.imsave('output_images/' + 'undistortedimage_withDetectedCorners.jpg', image_corners)
-#        # Choose offset from image corners to plot detected corners
-#        # This should be chosen to present the result at the proper aspect ratio
-#        # My choice of 100 pixels is not exact, but close enough for our purpose here
-#        offset = 100 # offset for dst points
-#        # Grab the image shape
-#        img_size = (gray.shape[1], gray.shape[0])
-#
-#        # For source points I'm grabbing the outer four detected corners
-#        src = np.float32([corners[0], corners[nx-1], corners[-1], corners[-nx]])
-#        # For destination points, I'm arbitrarily choosing some points to be
-#        # a nice fit for displaying our warped result 
-#        # again, not exact, but close enough for our purposes
-#        dst = np.float32([[offset, offset], [img_size[0]-offset, offset], 
-#                                     [img_size[0]-offset, img_size[1]-offset], 
-#                                     [offset, img_size[1]-offset]])
-#        # Given src and dst points, calculate the perspective transform matrix
-#        M = cv2.getPerspectiveTransform(src, dst)
-#        # Warp the image using OpenCV warpPerspective()
-#        warped = cv2.warpPerspective(undist, M, img_size)
-#
-#    # Return the resulting image and matrix
-#    return warped, M
+
 
 #img = mpimg.imread('camera_cal\calibration3.jpg')
 #top_down, perspective_M = corners_unwarp(image, nx, ny, mtx, dist)
@@ -207,29 +172,5 @@ for idx, filename in enumerate(testImages):
     
 #----------------------------------------
 ## detect lane lines
-# Define a class to receive the characteristics of each line detection
-class Line():
-    def __init__(self):
-        # was the line detected in the last iteration?
-        self.detected = False  
-        # x values of the last n fits of the line
-        self.recent_xfitted = [] 
-        #average x values of the fitted line over the last n iterations
-        self.bestx = None     
-        #polynomial coefficients averaged over the last n iterations
-        self.best_fit = None  
-        #polynomial coefficients for the most recent fit
-        self.current_fit = [np.array([False])]  
-        #radius of curvature of the line in some units
-        self.radius_of_curvature = None 
-        #distance in meters of vehicle center from the line
-        self.line_base_pos = None 
-        #difference in fit coefficients between last and new fits
-        self.diffs = np.array([0,0,0], dtype='float') 
-        #x values for detected line pixels
-        self.allx = None  
-        #y values for detected line pixels
-        self.ally = None
-
 
 ## determine lane curvature
